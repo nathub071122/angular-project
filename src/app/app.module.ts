de7397d225c677d3317a8bc8703f03b48b8096bc
nathub071122 <117662195+nathub071122@users.noreply.github.com>
2023-04-11 13:28:47 +0530

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 
 @NgModule({
   imports: [
@@ -19,6 +20,7 @@ import { ProductListComponent } from './product-list/product-list.component';
     AppComponent,
     TopBarComponent,
     ProductListComponent,
+    ProductAlertsComponent
   ],
   bootstrap: [
     AppComponent
